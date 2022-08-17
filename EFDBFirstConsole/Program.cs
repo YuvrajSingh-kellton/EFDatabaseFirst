6b48e52a143d5455be6cd4e006be87ee1ff74826
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-17 05:45:26 +0530

    Initial Commit

---
+++ EFDBFirstConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using EFDBFirst.Data;
+using EFDBFirst.Data.Models;
+
+namespace EFDBFirstConsole
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Employee employee = new Employee {EmpId = 2, EmpName = "Yuvraj Singh", EmpAddress = "Gurgaon" };
+            List<EmployeeEducation> Educationlist = new List<EmployeeEducation>();
+            Educationlist.Add(new EmployeeEducation { EducationName = "BTECH" });
+            CRUDoperations CRUDObj = new CRUDoperations();
+            //CRUDObj.InsertEmployeeAndItsEducation(employee, Educationlist);
+            //CRUDObj.DeleteEmployeeById(1);
+            //CRUDObj.ShowAllEmployee();
+            CRUDObj.UpdateEmployee(2, "Yuvraj Singh", "Haryana", new List<EmployeeEducation> { new EmployeeEducation { EducationName = "MTECH" } });
+            //CRUDObj.UpdateEmpEdueById(1, "BSC");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
