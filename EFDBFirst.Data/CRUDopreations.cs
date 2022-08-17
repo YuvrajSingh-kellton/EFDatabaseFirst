46e6fc8d731d8e8f9a4b242cde43568e5923e16c
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-17 05:43:57 +0530

    Initial Commit

---
+++ EFDBFirst.Data/CRUDopreations.cs
@@ -0,0 +1,101 @@
+﻿using EFDBFirst.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDBFirst.Data
+{
+    public class CRUDoperations
+    {
+        DBFirstContext DbFirst;
+        public CRUDoperations()
+        {
+            DbFirst = new DBFirstContext();
+        }
+
+        public void InsertEmployeeAndItsEducation(Employee employee, List<EmployeeEducation> EduList)
+        {
+            var addemp = new Employee
+            {
+                EmpId = employee.EmpId,
+                EmpName = employee.EmpName,
+                EmpAddress = employee.EmpAddress,
+                EmployeeEducations = EduList
+            };
+            DbFirst.Employees.Add(addemp);
+            DbFirst.SaveChanges();
+            Console.WriteLine("Employee Successfully Added...");
+        }
+        public void DeleteEmployeeById(int Id)
+        {
+            var employee = DbFirst.Employees.Where(emp => emp.EmpId == Id).Include(edu => edu.EmployeeEducations).FirstOrDefault();
+            if (employee != null)
+            {
+                employee.EmployeeEducations.Clear();
+                DbFirst.Employees.Remove(employee);
+                DbFirst.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("record not found...");
+            }
+        }
+
+        public void UpdateEmpEdueById(int Id, string eduName,int Empid)
+        {
+            var edu = DbFirst.EmployeeEducations.Where(emp => emp.EduId == Id).AsNoTracking().FirstOrDefault();
+            if (edu != null)
+            {
+                edu.EducationName = eduName;
+                edu.EmpId = Empid;
+                DbFirst.EmployeeEducations.Remove(edu);
+                DbFirst.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("record not found...");
+            }
+        }
+        public void UpdateEmployee(int id, string empname, string empaddress, List<EmployeeEducation> updateEducation)
+        {
+            var updateemp = DbFirst.Employees.Where(emp => emp.EmpId == id).Include(e => e.EmployeeEducations).AsNoTracking().FirstOrDefault();
+            if (updateemp != null)
+            {
+                updateemp.EmpName = empname;
+                updateemp.EmpAddress = empaddress;
+                updateemp.EmployeeEducations = updateEducation;
+                DbFirst.Employees.Update(updateemp);
+                DbFirst.SaveChanges();
+                Console.WriteLine("Updated...");
+            }
+            else
+            {
+                Console.WriteLine("No Record Available For The ID " + id);
+            }
+        }
+        public void ShowAllEmployee()
+        {
+            var getAllEmp = DbFirst.Employees.Include(e => e.EmployeeEducations).ToList();
+            if (getAllEmp != null)
+            {
+                foreach (var emp in getAllEmp)
+                {
+                    Console.WriteLine("Employee Name :" + emp.EmpName);
+                    Console.WriteLine("Employee Address : " + emp.EmpAddress);
+                    Console.WriteLine();
+                    Console.WriteLine("Employee Education :");
+                    foreach (var edu in emp.EmployeeEducations)
+                    {
+                        Console.WriteLine("     Employee Educations :" + edu.EducationName);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("----------------------------------------------------");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No Record Found...");
+            }
+        }
+    }
+}
---
+++ EFDBFirst.Data/Models/DBFirstContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFDBFirst.Data.Models
+{
+    public partial class DBFirstContext : DbContext
+    {
+        public DBFirstContext()
+        {
+        }
+
+        public DBFirstContext(DbContextOptions<DBFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<EmployeeEducation> EmployeeEducations { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-0P56O06;Database=DBFirst;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId)
+                    .HasName("PK__Employee__AF2DBB9921E65EA4");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmpId).ValueGeneratedNever();
+
+                entity.Property(e => e.EmpAddress)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<EmployeeEducation>(entity =>
+            {
+                entity.HasKey(e => e.EduId)
+                    .HasName("PK__Employee__1FD9490EC307B73D");
+
+                entity.ToTable("EmployeeEducation");
+
+                entity.Property(e => e.EduId).ValueGeneratedNever();
+
+                entity.Property(e => e.EducationName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpId).HasColumnName("EmpID");
+
+                entity.HasOne(d => d.Emp)
+                    .WithMany(p => p.EmployeeEducations)
+                    .HasForeignKey(d => d.EmpId)
+                    .HasConstraintName("FK__EmployeeE__EmpID__2E1BDC42");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
